d8cf8e8cfdd192e9e9158b12a446b32d4fd5f3bb
PARTHIBAN-007 <parthibanpk777@gmail.com>
2024-01-31 16:37:50 +0530

    first commit

--- commerce/urls.py
+++ commerce/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('register',views.register,name="register"),
+    path('login',views.login_page,name="login"),
     path('collection',views.collection,name="collection"),
     path('collection/<str:name>',views.collectionview,name="collection"),
+    path('collection/<str:cname>/<str:pname>',views.product_details,name="product_details"),
     
     
 ]
--- commerce/views.py
+++ commerce/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import redirect, render
 from . models import *
+from commerce.form import customeruserform
 from django.contrib import messages
-# Create your views here.
+
+
+
+# Create your views here
 def home(request):
-    return render(request,"shop/index.html")
+    products = Product.objects.filter(trending=1)
+    return render(request,"shop/index.html", {"products":products})
+
+
+def login_page(request):
+    return render(request,"shop/login.html")
 
 def register(request):
-    return render(request,"shop/register.html")
+    form=customeruserform()
+    if request.method=='POST':
+        form=customeruserform(request.POST) 
+        if form.is_valid():
+            form.save()
+            messages.success(request,"registeration success")
+            
+        
+    return render(request,"shop/register.html",{"form": form })
 
 def collection(request):
     catagory = Catagory.objects.filter(status = 1)
@@ -21,5 +38,16 @@ def collectionview(request,name):
         messages.warning(request,"no such category found")
         return redirect('collectiion')
         
-    
+def product_details(request,cname,pname):
+    if (Catagory.objects.filter(name=cname,status=1)):
+        if (Product.objects.filter(name=pname,status=1)):
+            products = Product.objects.filter(name=pname,status=1).first()
+            return render(request,"shop/products/productdetails.html", {"products":products})
+        else:
+            messages.error(request,"no such product is available")
+            return redirect('collectiion')
+    else:
+        messages.error(request,"no such Category is available") 
+        return redirect('collectiion')   
+        
     
